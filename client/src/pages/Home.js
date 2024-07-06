511ba76cd88300bccc79b56e5b8fd2a6ce8b219f
aditya19lohia <139976489+aditya19lohia@users.noreply.github.com>
2024-07-06 18:46:30 +0530

    History component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,13 +2,15 @@ import React from 'react'
 import Navbar from '../components/Navbar/Navbar'
 import Calendar from '../components/Navbar/Calendar'
 import TopBar from '../components/TopBar/TopBar'
+import History from '../components/History/History'
 function Home() {
   return (
     <div>
     
     <Navbar />
-    {/* <Calendar /> */}
+    <Calendar /> 
     <TopBar />
+    <History />
     
     </div>
   )
